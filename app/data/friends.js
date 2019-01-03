90843f44d26bc26a3d7c41f61893c0cde9b69c9a
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2019-01-03 15:30:05 -0500

    71%

---
+++ app/data/friends.js
@@ -0,0 +1,306 @@
+var friendstable=[
+    {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "test",
+    "photo": "test",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "teenarr",
+    "photo": "https://pbs.twimg.com/profile_images/1027665679254794241/mb4MBRUR_400x400.jpg",
+    "scores": [
+    "1",
+    "4",
+    "4",
+    "2",
+    "1",
+    "2",
+    "2",
+    "4",
+    "4",
+    "3"
+    ]
+    },
+    {
+    "name": "Eli",
+    "photo": "https://bqhfpnuv66xza-cdn-storage.azureedge.net/cache/d/6/b/c/2/a/d6bc2a848b16766ac353eef41ab557a582ac37c2.jpg",
+    "scores": [
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Bobby",
+    "photo": "pic",
+    "scores": [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "tate",
+    "photo": "https://www.gettyimages.ie/detail/illustration/fox-in-the-striped-t-shirt-royalty-free-illustration/466848925",
+    "scores": [
+    "1",
+    "3",
+    "3",
+    "4",
+    "2",
+    "4",
+    "5",
+    "5",
+    "5",
+    "3"
+    ]
+    },
+    {
+    "name": "Cooley",
+    "photo": "https://s3.amazonaws.com/simpson/tester_images/Cooley_profile.JPG",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "3",
+    "4",
+    "2",
+    "3",
+    "2",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "Cooley",
+    "photo": "https://s3.amazonaws.com/simpson/tester_images/Cooley_profile.JPG",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "3",
+    "4",
+    "2",
+    "3",
+    "2",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "Cooley",
+    "photo": "https://s3.amazonaws.com/simpson/tester_images/Cooley_profile.JPG",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "3",
+    "4",
+    "2",
+    "3",
+    "2",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "Vinci Bhardwaj",
+    "photo": "file:///Users/vincishar/Desktop/Screen%20Shot%202018-09-27%20at%208.17.42%20AM.png",
+    "scores": [
+    "5",
+    "3",
+    "5",
+    "4",
+    "5",
+    "1",
+    "5",
+    "3",
+    "4",
+    "4"
+    ]
+    },
+    {
+    "name": "asd",
+    "photo": "asd",
+    "scores": [
+    "3",
+    "2",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "asd",
+    "photo": "asd",
+    "scores": [
+    "3",
+    "2",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "asd",
+    "photo": "asd",
+    "scores": [
+    "3",
+    "2",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Milo",
+    "photo": "https://www.instagram.com/p/Bm1ZMPdHe2mffSM8I7JavxsgrQEVnuOxPgd3LA0/",
+    "scores": [
+    "1",
+    "1",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "4",
+    "4"
+    ]
+    }
+    ]
