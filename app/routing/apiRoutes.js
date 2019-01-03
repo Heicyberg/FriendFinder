8906ae82032d910f708ce4552a3cf8f0c450108b
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2019-01-03 01:06:43 -0500

    60%

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,19 @@
+var bodyParser = require('body-parser')
+// LOAD DATA
+var friendsData = require("../data/friends");
+// ROUTING
+module.exports = function(app) {
+    // API GET Requests
+  
+    app.get("/api/friends", function(req, res) {
+      res.json(friendsData);
+    });
+  
+    // API POST Requests
+  
+    app.post("/api/friends", function(req, res) {
+
+      friendsData.push(req.body);
+    });
+  
+  };
